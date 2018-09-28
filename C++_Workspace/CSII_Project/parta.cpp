c764410af0912b801d2303aeaa31e5a032410d9b
17robots <mdray@ameritech.net>
2018-09-28 08:40:59 -0400

    commit

--- C++_Workspace/CSII_Project/parta.cpp
+++ C++_Workspace/CSII_Project/parta.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <string>
+#include <stdlib.h>
+#include <time.h>
+#include <iomanip>
 
 const int NUMLABS = 8;
 const int LABSIZES[NUMLABS] = {19, 15, 24, 33, 61, 17, 55, 37};
@@ -45,6 +48,11 @@ class Station {
 	void operator==(Station &B); // meant to only compare the id's for the searching
 };
 
+int generateUserID() {
+  srand(time(NULL));
+  return rand() % 99999 + 1;
+}
+
 void printMenu() {
   std::cout << "Menu" << std::endl;
   std::cout << "1. Log In" << std::endl;
@@ -74,57 +82,132 @@ int main() {
 	std::cin >> menuChoice;
 
 	switch(menuChoice) {
-	  case 1:
-		for(int i = 0; i < NUMLABS; ++i) {
-		  std::cout << i + 1 << ". " << UNIVERSITYNAMES[i] << std::endl;
-		}
+	  case 1: {
+				for(int i = 0; i < NUMLABS; ++i) {
+				  std::cout << i + 1 << ". " << UNIVERSITYNAMES[i] << std::endl;
+				}
 
-		int uniChoice, StationChoice;
+				int uniChoice, stationChoice;
+				std::string Fname;
 
-		std::cout << "Which Uninversity are you logging into?: ";
-		std::cin >> uniChoice;
+				std::cout << "Which Uninversity are you logging into?: ";
+				std::cin >> uniChoice;
 
-		while(!(uniChoice < NUMLABS + 1 && uniChoice > 0)) {
-		  std::cout << "Please enter a number on the list: ";
-		  std::cin >> uniChoice;
-		}
-		for(int i = 0; i < LABSIZES[uniChoice - 1]; ++i) {
-		  std::cout << "[Station " << i + 1 << ": ";
-		  if(Labs[uniChoice - 1][i].getId() == -1) {
-			std::cout << "Open] ";
-		  } else {
-			std::cout << "In use by " << Labs[uniChoice - 1][i].getId() << "] ";
-		  }
-		}
-		std::cout << "Please choose a station (" << UNIVERSITYNAMES[uniChoice - 1] << "): ";
-		
-		break;
-	  case 2:
-		std::cout << "2. Log Off" << std::endl;
-		break;
+				while(!(uniChoice < NUMLABS + 1 && uniChoice > 0)) {
+				  std::cout << "Please enter a number on the list: ";
+				  std::cin >> uniChoice;
+				}
+				for(int i = 0; i < LABSIZES[uniChoice - 1]; ++i) {
+				  std::cout << "[Station " << i + 1 << ": ";
+				  if(Labs[uniChoice - 1][i].getId() == -1) {
+					std::cout << "Open] ";
+				  } else {
+					std::cout << "In use by " << std::setfill('0') << std::setw(5) << Labs[uniChoice - 1][i].getId() << ", " << Labs[uniChoice - 1][i].getName() << "] ";
+				  }
+				}
+				do {
+				  std::cout << "Please choose a station (" << UNIVERSITYNAMES[uniChoice - 1] << "): ";
+				  std::cin >> stationChoice;
+				  if(Labs[uniChoice - 1][stationChoice - 1].getId() != -1) { // if the computer is in use
+					std::cout << "That station is in use" << std::endl;
+					stationChoice = -1; // force the loop to repeat
+				  }
+				} while(!(stationChoice < LABSIZES[uniChoice - 1] && stationChoice > 0));
+
+				std::cout << "Now, how many minutes do you plan to use the computer for?: ";
+				int minutes;
+				std::cin >> minutes;
+
+				std::cout << "And finally, what is your name?: ";
+
+				getline(std::cin, Fname); // get rid of the enter character
+				getline(std::cin, Fname);
+
+				// now we log the user in 
+				Labs[uniChoice - 1][stationChoice - 1] = Station(generateUserID(), Fname, minutes);
+				std::cout << "Successfully Logged In " << Labs[uniChoice - 1][stationChoice - 1].getName() << " into " << UNIVERSITYNAMES[uniChoice - 1] << "'s Station " << stationChoice << " for " << minutes << " minutes." << std::endl;
+				break;
+			  }
+	  case 2: {
+				for(int i = 0; i < NUMLABS; ++i) {
+				  std::cout << i + 1 << ". " << UNIVERSITYNAMES[i] << std::endl;
+				}
+
+				int uniChoice, stationChoice;
+				std::string Fname;
+
+				std::cout << "Which Uninversity are you logging out of?: ";
+				std::cin >> uniChoice;
+
+				while(!(uniChoice < NUMLABS + 1 && uniChoice > 0)) {
+				  std::cout << "Please enter a number on the list: ";
+				  std::cin >> uniChoice;
+				}
+
+				for(int i = 0; i < LABSIZES[uniChoice - 1]; ++i) {
+				  std::cout << "[Station " << i + 1 << ": ";
+				  if(Labs[uniChoice - 1][i].getId() == -1) {
+					std::cout << "Open] ";
+				  } else {
+					std::cout << "In use by " << std::setfill('0') << std::setw(5) << Labs[uniChoice - 1][i].getId() << ", " << Labs[uniChoice - 1][i].getName() << "] ";
+				  }
+				}
+				do {
+				  std::cout << "Please choose a station (" << UNIVERSITYNAMES[uniChoice - 1] << "): ";
+				  std::cin >> stationChoice;
+				  if(Labs[uniChoice - 1][stationChoice - 1].getId() == -1) { // if the computer is not in use
+					std::cout << "That station is not in use" << std::endl;
+					stationChoice = -1; // force the loop to repeat
+				  }
+				} while(!(stationChoice < LABSIZES[uniChoice - 1] && stationChoice > 0));
+
+				// log the user out
+				std::string name = Labs[uniChoice - 1][stationChoice - 1].getName();
+				Labs[uniChoice - 1][stationChoice - 1] = Station();
+				std::cout << "Successfully Logged " << name << " from " << UNIVERSITYNAMES[uniChoice - 1] << "'s Station " << stationChoice << "." << std::endl;
+				break;
+			  }
 	  case 3: 
-		for(int i = 0; i < NUMLABS; ++i) {
-		  std::cout << "Computer Lab at " << UNIVERSITYNAMES[i] << "." << std::endl;
-		  for(int j = 0; j < LABSIZES[i]; ++j) {
-			std::cout << "[Station " << j + 1 << ": ";
-			if(Labs[i][j].getId() == -1) {
-			  std::cout << "Open] ";
-			} else {
-			  std::cout << "In use by " << Labs[i][j].getId() << "] ";
-			}
-		  }
-		  std::cout << std::endl << std::endl;
-		}
-		break;
-	  case 4:
-		std::cout << "4. Search" << std::endl;
-		break;
+			  for(int i = 0; i < NUMLABS; ++i) {
+				std::cout << "Computer Lab at " << UNIVERSITYNAMES[i] << "." << std::endl;
+				for(int j = 0; j < LABSIZES[i]; ++j) {
+				  std::cout << "[Station " << j + 1 << ": ";
+				  if(Labs[i][j].getId() == -1) {
+					std::cout << "Open] ";
+				  } else {
+					std::cout << "In use by " << std::setfill('0') << std::setw(5) << Labs[i][j].getId()<< ", " << Labs[i][j].getName() << "] ";
+				  }
+				}
+				std::cout << std::endl << std::endl;
+			  }
+			  break;
+	  case 4: {	
+				int idToSearch;
+				bool found = false;
+				std::cin >> idToSearch;
+				for(int i = 0; i < NUMLABS; ++i) {
+				  for(int j = 0; j < LABSIZES[i]; ++j) {
+					if(Labs[i][j].getId() == idToSearch) {
+					  found = true;
+					  std::cout << "Id: " << std::setfill('0') << std::setw(5) << Labs[i][j].getId() << std::endl;
+					  std::cout << "Name: " << Labs[i][j].getName() << std::endl;
+					  std::cout << "University: " << UNIVERSITYNAMES[i] << std::endl;
+					  std::cout << "Station: " << j + 1 << std::endl;
+					  std::cout << "Expected Use Time: " << Labs[i][j].getUseTime() << std::endl;
+					}
+				  }
+				}
+				if(!found) {
+				  std::cout << "That user is not logged in." << std::endl;
+				}
+				break;
+			  }
 	  case 5:
-		break;
+			  break;
 	  default:
-		std::cout << "That was not a valid option please try again" << std::endl;
-		std::cin >> menuChoice;
-		break;
+			  std::cout << "That was not a valid option please try again" << std::endl;
+			  std::cin >> menuChoice;
+			  break;
 	}
   } while(menuChoice != 5);
 
