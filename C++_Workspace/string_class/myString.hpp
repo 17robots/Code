83fd0336026681536eff47347504d5536d82c5f4
17robots <mdray@ameritech.net>
2018-10-09 16:53:45 -0400

    message

--- C++_Workspace/string_class/myString.hpp
+++ C++_Workspace/string_class/myString.hpp
@@ -1,16 +1,23 @@
 #ifndef MYSTRING_HPP
 #define MYSTRING_HPP
+#include <iostream>
 
 // lets define things
 namespace umm {
   class myString {
-  	public:
+	public:
 	  myString(); // default construtor 
 	  myString(char const * str); // the string constructor
 	  myString(const myString &obj); // the copy constructor
 	  ~myString(); // deconstructor
+	  int length() const;
 	  myString & operator=(const myString &obj); // operator override
-	  char * getStringArray();
+	  char * getStringArray() const;
+	  char * getLastChar() const;
+	  char * getMaxChar() const;
+	  bool empty() const;
+	  friend std::ostream & operator <<(std::ostream& os, const umm::myString &obj);
+
 	private:
 	  // we will need to create a char array that is stored on the heap so that
 	  // we can allocate more space as necessary.
@@ -18,6 +25,8 @@ namespace umm {
 	  char * lastChar; // the last char of the string
 	  char * maxChar; // the last char of the string + 1
   };
+
+  std::ostream &operator<<(std::ostream &os, const umm::myString &obj);
 }
 
 #endif
--- C++_Workspace/string_class/testing.cpp
+++ C++_Workspace/string_class/testing.cpp
@@ -6,6 +6,19 @@
 #include <cassert>
 
 int main() {
-    
-    return 0;
+  umm::myString s;
+  char const * str = "hello world"; 
+  umm::myString yes = str;
+  assert(yes.getStringArray() != str);
+  assert(strcmp(yes.getStringArray(), str) == 0);
+  std::cout << yes << std::endl; 
+  // std::cout << yes.length();
+  s = yes;
+  // std::cout << s.length();
+  umm::myString const s1;
+  assert(s1.empty());
+
+  umm::myString const s2 = "";
+  assert(s2.empty());
+  return 0;
 }
