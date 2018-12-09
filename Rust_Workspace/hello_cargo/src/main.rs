3e028b456d01fe629da485d410f10a1052923219
17robots <mdray@ameritech.net>
2018-12-09 12:24:34 -0500

    something with rust

--- Rust_Workspace/hello_cargo/src/main.rs
+++ Rust_Workspace/hello_cargo/src/main.rs
@@ -1,12 +1,3 @@
-use std::io;
-
 fn main() {
-    println!("Welcome to the Guesing Game");
-    println!("Please Enter Your Guess");
-
-    let mut guess = String::new();
-
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
-
-    println!("You guessed {}", guess);
+    println!("Hello, world!");
 }
--- Rust_Workspace/hello_cargo/target/debug/build/winapi-307370f32ca02752/build_script_build-307370f32ca02752.d
+++
@@ -1,5 +0,0 @@
-C:\Users\mdray\Documents\Code\Rust_Workspace\hello_cargo\target\debug\build\winapi-307370f32ca02752\build_script_build-307370f32ca02752.exe: C:\Users\mdray\.cargo\registry\src\github.com-1ecc6299db9ec823\winapi-0.3.5\build.rs
-
-C:\Users\mdray\Documents\Code\Rust_Workspace\hello_cargo\target\debug\build\winapi-307370f32ca02752\build_script_build-307370f32ca02752.d: C:\Users\mdray\.cargo\registry\src\github.com-1ecc6299db9ec823\winapi-0.3.5\build.rs
-
-C:\Users\mdray\.cargo\registry\src\github.com-1ecc6299db9ec823\winapi-0.3.5\build.rs:
