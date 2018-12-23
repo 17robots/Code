1cb826de00b0bcc8df8b9a5158fbcca8f4dee4a6
17robots <mdray@ameritech.net>
2018-12-22 19:17:55 -0500

    text editor project and can now switch between pics and text results

---
+++ JavaScript_Workspace/slate-text-editor/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { TextEditor } from './components';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <TextEditor />
+      </div>
+    );
+  }
+}
+
+export default App;
