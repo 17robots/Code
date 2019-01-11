d52754dcbe41ce3efbd389ad9e292ec4dd3a42b3
Matthew Dray <MEDray@drbsystems.com>
2019-01-11 15:24:09 -0500

    made redux changes

---
+++ JavaScript_Workspace/search_v5/client/redux/reducer.js
@@ -0,0 +1,14 @@
+import * as Constants from './constants'
+
+let initial = {
+    searched: false,
+    searchText: '',
+    results: [],
+    resultType: ''
+}
+
+export default reducer = (state = initial, action) => {
+    switch(action.type) {
+        case Constants.
+    }
+}
