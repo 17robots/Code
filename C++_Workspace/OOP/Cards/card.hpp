85fcbc39f3357800d523ce7d9423829b303c8751
Matthew Dray <MEDray@drbsystems.com>
2019-01-22 17:21:39 -0500

    more cleaning

--- C++_Workspace/OOP/Cards/card.hpp
+++ C++_Workspace/OOP/Cards/card.hpp
@@ -1,9 +1,34 @@
 #pragma once
+#ifndef CARD_HPP
+#define CARD_HPP
 
+private:
 enum Suit {
-    
+    Clubs,
+    Diamonds,
+    Hearts,
+    Spades
 };
 
 enum Rank {
-
+    A,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King
 };
+
+public:
+struct Card {
+    Suit suit;
+    Rank rank;
+}
+
