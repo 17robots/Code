084926e6d02b65fb9b4376684169c6247f113c91
17robots <mdray@ameritech.net>
2019-02-12 21:05:31 -0500

    OOP Commit 02/12/19: [3460:421-001] Adding the bit manipulation notes and restarting the project

---
+++ C++_Workspace/OOP/Bit_Ops_Notes/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+	char test = 191;
+	int testInt = test & 0x3f; // grabs the last 6 bits from the right
+	std::cout << testInt << std::endl;
+	testInt = ((test >> 6) & 0x3); // grabs the first 2 bits from the left
+	std::cout << testInt << std::endl;
+	// rank = last 6 
+	// suit = first 2
+	return 0;
+}
