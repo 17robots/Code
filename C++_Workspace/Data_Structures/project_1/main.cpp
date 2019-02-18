7a282ecc890ae9b3c9cac3ecd6101e6effa2bd22
Matthew Dray <mdray@pop-os.localdomain>
2019-02-18 01:16:50 -0500

    Data Structures adding more logic and fixing errors for the sort

--- C++_Workspace/Data_Structures/project_1/main.cpp
+++ C++_Workspace/Data_Structures/project_1/main.cpp
@@ -7,7 +7,9 @@
 #include <sstream>
 
 std::list<int>::iterator gt(std::list<int>::iterator start, std::list<int>::iterator stop, int x) {
-
+    std::list<int>::iterator test = start;
+    return test;
+		// implement this later
 }
 
 void swap(int i,int j, std::vector<int>& a){
@@ -36,7 +38,7 @@ int partition(std::vector<int>& a, int i, int j) {
 void sort(std::vector<int>& sortThis, int lower, int upper) {
 	if (lower < upper) 
     { 
-        /* pi is partitioning index, arr[p] is now 
+        /* pi is partitioning index, sortThis[pi] is now 
            at right place */
         int pi = partition(sortThis, lower, upper); 
   
@@ -57,11 +59,13 @@ void printAdjList(std::vector<std::list<int>>& adjList) {
 }
 
 bool connComponent(const std::list<int> & com1, const std::list<int> & com2) {
-
+  return true;
+	// implement this later
 }
 
 bool merge(std::list<int> & list1, std::list<int> & list2) {
-
+  return true;
+	// implement this later
 }
 
 int main(int argc, char* argv[], char* envp[]) {
@@ -98,13 +102,20 @@ int main(int argc, char* argv[], char* envp[]) {
 			sortMe.push_back(n);
 		}
 		sort(sortMe, 0, sortMe.size() - 1);
-		graphList.push_back(std::list<int>({ std::begin(sortMe), std::end(sortMe) })); // copy the newly-sorted vector as a list into the graphList vector
+		graphList.push_back(std::list<int>({ std::begin(sortMe), std::end(sortMe) })); // copy the sorted vector as a list into the graphList vector
 		++counter; // next index
 	}
 
 	std::cout << "List read. Printing Adjacency List:\n";
 
 	printAdjList(graphList);
+
+	// logic for menu loop interface for the merging of lists
+	
+
+	// final printout logic
+	
+
 
    return 0;
 }
