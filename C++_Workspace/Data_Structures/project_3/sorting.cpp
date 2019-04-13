80d222e04b02f207ece77feeb082f6044c2d52b2
Matthew Dray <mdray@ameritech.net>
2019-04-13 18:22:44 -0400

    Update sorting.cpp

--- C++_Workspace/Data_Structures/project_3/sorting.cpp
+++ C++_Workspace/Data_Structures/project_3/sorting.cpp
@@ -17,9 +17,36 @@ int main() {
         std::cerr << "File not found.";
         return 1;
     }
-    std::istringstream sstream(file);
+    
+    std::vector<int> numbers;
+    std::string line;
+    
+    while(getline(file, line)) {
+        std::istringstream sstream(line);
+        int number;
+        sstream >> number;
+        numbers.push_back(number);
+    }
 
-    std::cout << "File loaded. Please enter a number of elements: " << 
+    std::cout << "File vector loaded. Please enter a number of elements to generate: ";
+    
+    int elememts;
+    
+    std::cin >> elememnts;
+    
+    while(cin.fail()) {
+        std::cout << "Please enter a number: ";
+        std::cin >> elements;
+    }
+    
+    // create 3 vectors
+    std::vector<int> ascending;
+    std::vector<int> descending;
+    std::vectot<int> random;
+    
+    for(int i = 0; i < elements; ++i) {
+        
+    }
 
     return 0;
 }
