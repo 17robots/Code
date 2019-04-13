b47da37310619a69b18daf05f221deed28a50530
17robots <mdray@ameritech.net>
2019-04-13 13:03:13 -0400

    Add more to driver program

--- C++_Workspace/Data_Structures/project_3/sorting.cpp
+++ C++_Workspace/Data_Structures/project_3/sorting.cpp
@@ -2,7 +2,24 @@
 #include "quicksort.h"
 #include "heapsort.h"
 #include "insertsort.h"
+#include <string>
+#include <fstream>
+#include <sstream>
 
 int main() {
+    std::cout << "Sorting Program\n";
+    std::cout << "Please enter the name of a file: ";
+    std::string filename; 
+    std::cin >> filename;
+    std::ifstream file(filename);
+
+    if(!file) {
+        std::cerr << "File not found.";
+        return 1;
+    }
+    std::istringstream sstream(file);
+
+    std::cout << "File loaded. Please enter a number of elements: " << 
+
     return 0;
 }
