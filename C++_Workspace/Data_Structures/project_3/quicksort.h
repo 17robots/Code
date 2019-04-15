81a5960701dee40c82bf2338249f2ece4ae29903
Matthew Dray <mdray@ameritech.net>
2019-04-15 14:16:39 -0400

    Update Quicksort.h

--- C++_Workspace/Data_Structures/project_3/quicksort.h
+++ C++_Workspace/Data_Structures/project_3/quicksort.h
@@ -25,9 +25,10 @@ const Comparable& median3(std::vector<Comparable> &a, int left, int right) {
 template <typename Comparable>
 void quickSort(std::vector<Comparable> &a, int left, int right) {
     // removed the if statement condition from before and changed to the one below
-    if(a.size() >= 1) {
+    if(a.size() > 1) {
         const Comparable& pivot = median3(a, left, right);
         
+        // partitioning 
         int i = left, j = right - 1;
         for(; ;) {
             while(a[++i] < pivot) { }
