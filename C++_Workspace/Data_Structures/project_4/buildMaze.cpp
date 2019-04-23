ef07566694fac5192574ba43fdcf31788d8b7cf2
17robots <mdray@ameritech.net>
2019-04-23 13:49:50 +0000

    Add printMaze for Data Structures Project 4

--- C++_Workspace/Data_Structures/project_4/buildMaze.cpp
+++ C++_Workspace/Data_Structures/project_4/buildMaze.cpp
@@ -1,1 +1,33 @@
-int main() { return 0; }
+#include <iostream>
+#include "maze.h"
+#include "DisjSets.h"
+
+
+int main() {
+    std::cout << "============= Maze Builder =============\n";
+    std::cout << "Enter the number of rows in the maze (greater than 1): ";
+    
+    int rows;
+    std::cin >> rows;
+    
+    while(std::cin.fail() || rows <= 1) {
+        std::cout << "Please make sure your row is a number  greater than 1.\n";
+        std::cout << "Enter the number of rows in the maze (greater than 1): ";
+        std::cin >> rows;
+    }
+    
+    std::cout << "Enter the number of columns in the maze (greater than 1): ";
+    
+    int cols;
+    std::cin >> cols;
+    
+    while(std::cin.fail() || rows <= 1) {
+        std::cout << "Please make sure your column is a number  greater than 1.\n";
+        std::cout << "Enter the number of columns in the maze (greater than 1): ";
+        std::cin >> cols;
+    }
+    
+    maze(rows, cols).printMaze();
+    
+    return 0;
+}
--- C++_Workspace/Data_Structures/project_4/maze.cpp
+++ C++_Workspace/Data_Structures/project_4/maze.cpp
@@ -1,9 +1,56 @@
-<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
-<html><head>
-<title>404 Not Found</title>
-</head><body>
-<h1>Not Found</h1>
-<p>The requested URL /~dforeback/classes/ds/Projects/Project4/maze.cpp was not found on this server.</p>
-<hr>
-<address>Apache/2.4.7 (Ubuntu) Server at www.cs.uakron.edu Port 80</address>
-</body></html>
+#include "maze.h"
+
+maze::maze(int r, int c) {
+    row = r;
+    col = c;
+    theMaze = std::vector<mazeCell>(r * c);
+    theMaze.at(0).setTop(false);
+    theMaze.at(0).setLeft(false);
+    theMaze.at(row * col - 1).setRight(false);
+    theMaze.at(row * col - 1).setBot(false);
+} // constructor
+
+// returns true if neigh is cell's neighbor
+//(i.e. in position left, right, top or bot
+// with respect to cell)
+bool maze::neighbors(int cell, int neigh) const {
+    
+}
+
+// get rid of cell's wall between cell and neighbor
+// Note: this method should be invoked twice from the driver
+// code to smashWall(a,b) and to smashWall(b,a)
+void maze::smashWall (int cell, int neigh) {
+    
+}
+
+//print the maze
+void maze::printMaze() {
+    // top row
+    for(int j = 0; j < col; ++j) {
+        if(theMaze.at(j).getTop()) {
+            std::cout << "_ ";
+        } else {
+            std::cout << "  ";
+        }
+    }
+    std::cout << '\n';
+    
+    for(int i = 0; i < theMaze.size(); ++i) {
+        if(i % col == 0 && i != 0) {
+            std::cout << "|\n";
+        }
+        
+        if(theMaze.at(i).getLeft() && theMaze.at(i).getBot()) {
+            std::cout << "|_";
+        } else if(theMaze.at(i).getLeft()) {
+            std::cout << "| ";
+        } else if(theMaze.at(i).getBot()) {
+            std::cout << " _";
+        } else {
+            std::cout << "  "; 
+        }
+    }
+    
+    std::cout << '\n';
+}
